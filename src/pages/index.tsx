d75c28d29a514c6ec995d5613eef0ba50ce74bfd
Ssnnee <nandisamuelsne@gmail.com>
2024-03-11 13:49:56 +0100

    feat: put the header on the right place

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import { type NextPage } from "next";
 import Head from "next/head";
 // import Link from "next/link";
 
-import { HiOutlineMenuAlt2 } from "react-icons/hi";
-import Sidebar from "../components/Sidebar";
 
 import { api } from "../utils/api";
 
@@ -21,15 +19,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <header className='bg-primary top-0 z-50 w-full justify-between text-white border-b border-gray-700 backdrop-blur'>
-      <div className='container mx-auto flex gap-5 items-center p-4'>
-        <button>
-            <HiOutlineMenuAlt2 className='text-3xl' onClick={() => setIsOpen(!isOpen)} />
-        </button>
-        <h1 className='text-2xl font-bold '>CelloTree</h1>
-      </div>
-    </header>
-    <Sidebar isOpen={isOpen} />
 
       <main className="grid grid-cols-sidbar min-h-screen text-white  bg-primary ">
       {/* <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
