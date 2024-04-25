23056d6a680facc6da5cbd887cb30765bac548f2
Samuel Nandi <nandisamuelsne@gmail.com>
2024-04-25 12:26:01 +0100

    feat: add bg to div when the are hover or clicked

--- src/app/_components/Tree.tsx
+++ src/app/_components/Tree.tsx
@@ -3,18 +3,25 @@ import Link from "next/link";
 import { Separator } from "~/components/ui/separator";
 import TreeActions from "./TreeActions";
 import { TreeRefetchHook } from "./TreeRefetchHook";
+import { useState } from "react";
 
 export default function Tree() {
 
   const{ userAccess } = TreeRefetchHook()
+  const [ isClicked, setIsClicked ] = useState<string | null>(null)
 
 
   return (
     <div className="">
       {userAccess.data?.map((access, index) => (
-        <div key={access.id} className="flex flex-col p-4">
-          <div className="flex justify-between items-center">
-            <Link href={`/tree/${access.tree.id}`} className="w-full">
+        <div key={access.id} className="flex flex-col">
+          <div
+            className={`flex justify-between items-center rounded p-2
+              hover:bg-accent hover:text-accent-foreground
+              ${isClicked === access.id && "bg-accent text-accent-foreground"}`} // Check if the current div's ID matches isClicked
+            onClick={() => setIsClicked(isClicked === access.id ? null : access.id)} // Toggle the clicked state based on the current ID
+          >
+            <Link href={`/tree/${access.tree.id}`} className="w-full ">
              {access.tree.name}
             </Link>
             <div className="cursor-pointer">
