3f2d6ea575ce8b7cbbd5e3872b1bf96da039861f
Samuel Nandi <nandisamuelsne@gmail.com>
2024-06-06 20:15:11 +0100

    feat: add family visualiser

---
+++ src/app/(app)/view/[tree]/page.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import React from "react";
+import { FamilyChart } from "./tree-view-example";
+import { api } from "~/trpc/react";
+import { format } from "date-fns";
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading
+} from "~/app/_components/page-header";
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
+
+interface Base {
+  id: string;
+  firstname: string | null;
+  lastname: string;
+  birthdate: Date | null;
+  avatarURL: string | null;
+  sex: string;
+  fatherId: string | null;
+  motherId: string | null;
+  description: string | null;
+}
+
+interface Member extends Base {
+  spouses: Base[];
+  spouseOf: Base[];
+  fatherChildren: Base[];
+  motherChildren: Base[];
+}
+
+interface Data {
+  id: string;
+  rels: {
+    spouses?: string[];
+    children?: string[];
+    father?: string;
+    mother?: string;
+  };
+  data: {
+    "first name"?: string;
+    "last name"?: string;
+    birthday?: string;
+    avatar?: string;
+    gender?: string;
+    description?: string;
+  };
+}
+
+export default function ViewPage({ params }: { params: { tree: string } }) {
+  const treeMemberQuery = api.member.getMembersByTreeId.useQuery({ id: params.tree });
+  const treeQuery = api.tree.getById.useQuery({ id: params.tree });
+
+  if (treeMemberQuery.isLoading || treeQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (treeMemberQuery.isError || treeQuery.isError || !treeMemberQuery.data || !treeQuery.data) {
+    return <div>Error loading data</div>;
+  }
+
+  const members = treeMemberQuery.data.member;
+  const treeName = treeQuery.data.name;
+  const data = transformData(members);
+
+  return (
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>
+          Découvrez les membres de la famille {treeName} en visualisant l'arbre généalogique
+        </PageHeaderHeading>
+        <PageHeaderDescription>
+          Ceci est la liste des membres de la famille {treeName}. Vous pouvez
+          retrouver de manière visuelle les informations des membres ayant des
+          parents renseignés.
+        </PageHeaderDescription>
+        <PageActions>
+          <Button asChild>
+            <Link href={`/member/${params.tree}`}>
+              Voir la liste des membres et plus d'informations.
+            </Link>
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <FamilyChart data={data} />
+    </div>
+  );
+}
+
+function transformData(apiData: Member[]): Data[] {
+  const membersMap = new Map<string, Data>();
+
+  apiData.forEach(member => {
+    membersMap.set(member.id, {
+      id: member.id,
+      rels: {
+        spouses: member.spouses.length > 0
+          ? member.spouses.map(spouse => spouse.id)
+          : member.spouseOf.map(spouse => spouse.id) ?? [],
+        father: member.fatherId ?? undefined,
+        mother: member.motherId ?? undefined,
+        children: member.sex === "M"
+          ? member.fatherChildren.map(child => child.id)
+          : member.motherChildren.map(child => child.id) ?? []
+      },
+      data: {
+        "first name": member.firstname ?? "",
+        "last name": member.lastname,
+        birthday: member.birthdate ? format(new Date(member.birthdate), "dd MMM yyyy") : "",
+        avatar: member.avatarURL ?? "",
+        gender: member.sex,
+        description: member.description ?? ""
+      }
+    });
+  });
+
+  apiData.forEach(member => {
+    if (member.fatherId) {
+      const father = membersMap.get(member.fatherId);
+      if (father && !father.rels.children?.includes(member.id)) {
+        father.rels.children?.push(member.id);
+      }
+    }
+    if (member.motherId) {
+      const mother = membersMap.get(member.motherId);
+      if (mother && !mother.rels.children?.includes(member.id)) {
+        mother.rels.children?.push(member.id);
+      }
+    }
+  });
+
+  return Array.from(membersMap.values());
+}
---
+++ src/app/(app)/view/[tree]/tree-view-example.tsx
@@ -0,0 +1,90 @@
+import f3 from "family-chart";
+import { useEffect, useRef } from "react";
+import { toast } from "~/components/ui/use-toast";
+
+interface Data {
+  id: string;
+  rels: {
+    spouses?: string[];
+    children?: string[];
+    father?: string;
+    mother?: string;
+  };
+  data: {
+    "first name"?: string;
+    "last name"?: string;
+    birthday?: string;
+    avatar?: string;
+    gender?: string;
+    description?: string;
+  };
+}
+
+export const FamilyChart: React.FC<{ data: Data[] }> = ({ data }) => {
+  const cont = useRef<HTMLDivElement>(null);
+
+  const validateData = (data: Data[]) => {
+    if (!Array.isArray(data)) {
+      return "Data must be an array.";
+    }
+    for (const item of data) {
+      if (!item.id || typeof item.id !== "string") {
+        return "Each data item must have an 'id' of type string.";
+      }
+      if (!item.data) {
+        return "Each data item must have a 'data' object.";
+      }
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    if (!cont.current) return;
+
+    const error = validateData(data);
+    if (error) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: error,
+      });
+      return;
+    }
+
+    try {
+      const store = f3.createStore({
+          data: data,
+          node_separation: 250,
+          level_separation: 150,
+        }),
+        view = f3.d3AnimationView({
+          store,
+          cont: cont.current,
+        }),
+        Card = f3.elements.Card({
+          store,
+          svg: view.svg,
+          card_dim: { w: 220, h: 70, text_x: 75, text_y: 15, img_w: 60, img_h: 60, img_x: 5, img_y: 5 },
+          card_display: [
+            (d: Data) => `${d.data["first name"] || ""} ${d.data["last name"] || ""}`,
+            (d: Data) => `${d.data["description"] || ""}`,
+            (d: Data) => `${d.data["birthday"] || ""}`,
+          ],
+          mini_tree: true,
+          link_break: true,
+        });
+
+      view.setCard(Card);
+      store.setOnUpdate((props: any) => view.update(props || {}));
+      store.update.tree({ initial: true });
+    } catch (e) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Il semble qu'il y ait un problème.",
+        description: "Les données fournies ne sont pas valides. Assurez-vous que les conjoint sont ajoutés et réessayez.",
+      });
+    }
+  }, [data]);
+
+  return <div className="f3" id="FamilyChart" ref={cont}></div>;
+};
